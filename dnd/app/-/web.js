1ceb054f6b79d03dbcb304fbd577568a614094c9
Dimonchester <dimonchester.tkachov@gmail.com>
2025-11-15 19:54:07 +0300

    update

--- dnd/app/-/web.js
+++ dnd/app/-/web.js
@@ -11573,13 +11573,6 @@ var $;
 "use strict";
 var $;
 (function ($) {
-    $mol_style_attach("my/dnd/notes/notes.view.css", "\n[mol_check]:not([mol_check_checked]) > [mol_check_box_icon] {\n\tfill: transparent;\n}\n\n[mol_check]:not([disabled]) > [mol_check_box_icon] {\n\tbackground: var(--mol_theme_field);\n\tcolor: var(--mol_theme_text);\n}\n");
-})($ || ($ = {}));
-
-;
-"use strict";
-var $;
-(function ($) {
     var $$;
     (function ($$) {
         $mol_style_define($my_dnd_notes, {
